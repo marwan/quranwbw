88950819781a0a3752f57a9761a6bf8ba51351e3
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-03-12 23:17:12 +0530

    added Nastaleeq Hafs Bold font

--- src/data/options.js
+++ src/data/options.js
@@ -15,7 +15,8 @@ export const selectableFontTypes = {
 	4: { id: 4, font: 'Indopak Madinah', apiId: 3, disallowedIn: ['mushaf'] },
 	5: { id: 5, font: 'Nastaleeq Hafs Digital', apiId: 5, disallowedIn: ['mushaf'] },
 	6: { id: 6, font: 'Indopak Hanafi', apiId: 6, disallowedIn: ['mushaf'] },
-	7: { id: 7, font: 'Uthmanic Hafs Digital (Bold)', apiId: 1, disallowedIn: ['mushaf'] }
+	7: { id: 7, font: 'Uthmanic Hafs Digital (Bold)', apiId: 1, disallowedIn: ['mushaf'] },
+	8: { id: 8, font: 'Nastaleeq Hafs Digital (Bold)', apiId: 5, disallowedIn: ['mushaf'] }
 };
 
 export const themeColors = ['light', 'dark'];
