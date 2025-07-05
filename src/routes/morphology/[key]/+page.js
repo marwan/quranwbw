7dcf853bb18d4bb46c372677f65769c864f7c996
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-05 22:21:54 +0530

    Update +page.js

--- src/routes/morphology/[key]/+page.js
+++ src/routes/morphology/[key]/+page.js
@@ -1,11 +1,12 @@
 import { error } from '@sveltejs/kit';
 import { isValidVerseKey, isValidWordKey } from '$utils/validateKey';
 
-export async function load({ params }) {
-	const key = params.key;
+export async function load({ params, url }) {
+	// Try to get `key` from query param `word` or from the route param
+	const key = url.searchParams.get('word') || params.key;
 
-	if (!isValidVerseKey(key) && !(await isValidWordKey(key))) {
-		error(404, {
+	if (!key || (!isValidVerseKey(key) && !(await isValidWordKey(key)))) {
+		throw error(404, {
 			message: 'Not found'
 		});
 	}
