cc7a4616fc41cf5542a95af4700981a78d19127c
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-06 18:21:51 +0530

    styling fix

--- src/components/ui/Modals/LoginModal.svelte
+++ src/components/ui/Modals/LoginModal.svelte
@@ -15,12 +15,15 @@
 </script>
 
 <Modal title="Login" id="loginModal" bind:open={$__loginModalVisible} class="rounded-3xl" bodyClass="p-6 space-y-4 flex-1 overflow-y-auto overscroll-contain" headerClass="flex justify-between items-center p-6 rounded-t-3xl" center outsideclose>
-	<div class="flex flex-col space-y-4 text-sm">
+	<div class="flex flex-col items-center space-y-4 text-sm text-center">
 		{#if session}
-			<p>Welcome, {session.user.email}</p>
-			<button on:click={logout}>Logout</button>
+			<div class="flex flex-col items-center space-y-2">
+				<img src={session.user.user_metadata.avatar_url} alt="User Avatar" class="w-12 h-12 rounded-full object-cover shadow" referrerpolicy="no-referrer" />
+				<p class="text-base font-medium">Welcome, {session.user.user_metadata.name}</p>
+				<button on:click={logout} class="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-lg text-sm"> Logout </button>
+			</div>
 		{:else}
-			<button on:click={loginWithGoogle}>Login with Google</button>
+			<button on:click={loginWithGoogle} class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg text-sm"> Login with Google </button>
 		{/if}
 	</div>
 </Modal>
