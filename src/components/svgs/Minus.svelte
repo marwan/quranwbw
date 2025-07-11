080d4511973be551414ff21b498c07cd7aeae936
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-11 22:41:17 +0530

    removed unused svgs

--- src/components/svgs/Minus.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let size = 4;
-</script>
-
-<svg class="w-{size} h-{size}" aria-hidden="true" fill="none" viewBox="0 0 18 2">
-	<path stroke={window.theme('icon')} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
-</svg>
