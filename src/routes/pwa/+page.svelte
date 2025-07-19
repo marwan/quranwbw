e7617d21c33642c67ed7e920991f9dd59bd4f4f3
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-20 01:01:59 +0530

    changes

--- src/routes/pwa/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Quran from '$svgs/Quran.svelte';
-</script>
-
-<div class="w-[512px] h-[512px] overflow-hidden homepage-background-image {window.theme('bgSecondaryLight')}">
-	<div class="flex items-center justify-center w-full h-full">
-		<Quran size="size-80" />
-	</div>
-</div>
