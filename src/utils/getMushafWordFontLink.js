765dd82c7be141bb495cbfd19d4b2d809b6c88c3
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-23 18:11:34 +0530

    trying out ot-svg-fonts

--- src/utils/getMushafWordFontLink.js
+++ src/utils/getMushafWordFontLink.js
@@ -1,6 +1,21 @@
-import { mushafWordFontLink, mushafFontVersion } from '$data/websiteSettings';
+import { get } from 'svelte/store';
+import { __websiteTheme } from '$utils/stores';
+
+const themeDirs = {
+	1: { dir: 'OT-SVG-L' },
+	2: { dir: 'OT-SVG-L' },
+	3: { dir: 'OT-SVG-L' },
+	4: { dir: 'OT-SVG-S' },
+	5: { dir: 'OT-SVG-D' },
+	6: { dir: 'OT-SVG-D' },
+	7: { dir: 'OT-SVG-D' },
+	8: { dir: 'OT-SVG-D' },
+	9: { dir: 'OT-SVG-D' }
+};
 
 // Return the URL for the mushaf font by page
 export function getMushafWordFontLink(page) {
-	return `${mushafWordFontLink}/QCF4${`00${Number(page)}`.slice(-3)}_COLOR-Regular.woff2?version=${mushafFontVersion}`;
+	const newLink = `https://ot-svg-fonts.pages.dev`;
+	const dir = themeDirs[get(__websiteTheme)].dir;
+	return `${newLink}/${dir}/QCF4${`00${Number(page)}`.slice(-3)}_COLOR-Regular.woff2?version=1`;
 }
