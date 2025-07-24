b2ddda4b8ce5c59750c6093f8852b09fe5a890da
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-24 20:13:36 +0530

    parallel fetching in morphology page, updated waiting messages in spinner

--- src/components/svgs/Spinner.svelte
+++ src/components/svgs/Spinner.svelte
@@ -9,9 +9,11 @@
 	let timeouts = [];
 
 	const messages = [
-		{ delay: 2000, text: 'Hang tight, loading some heavy data...' },
-		{ delay: 4000, text: 'Still working… almost there.' },
-		{ delay: 8000, text: 'Thanks for your patience, large files take a bit longer!' }
+		{ delay: 2000, text: 'Just a moment…' },
+		{ delay: 6000, text: 'Still loading, hang in there.' },
+		{ delay: 12000, text: 'Almost done… thanks for waiting!' },
+		{ delay: 20000, text: 'Still working… slow internet might be the reason.' },
+		{ delay: 30000, text: 'Hmm, something might’ve gone wrong. You can try refreshing.' }
 	];
 
 	function resetMessages() {
@@ -33,12 +35,12 @@
 	});
 </script>
 
-<div class="flex m-auto h-{height} {margin} justify-center items-center text-center">
+<div class="flex flex-col m-auto h-{height} {margin} justify-center items-center text-center">
 	<svg class="animate-spin w-{size} h-{size}" fill="none" viewBox="0 0 32 32">
 		<path clip-rule="evenodd" d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z" fill={window.theme('icon')} fill-rule="evenodd" />
 	</svg>
 
 	{#if message}
-		<p class="text-sm">{message}</p>
+		<p class="text-xs">{message}</p>
 	{/if}
 </div>
--- src/routes/morphology/Table.svelte
+++ src/routes/morphology/Table.svelte
@@ -1,16 +1,10 @@
 <script>
 	export let wordKeys = [];
 	export let tableType;
+	export let wordData;
 
 	import { buttonClasses, linkClasses } from '$data/commonClasses';
 	import { term } from '$utils/terminologies';
-	import { fetchWordData } from '$utils/fetchData';
-	import { __wordTranslation, __wordTransliteration } from '$utils/stores';
-
-	$: wordData = (async () => {
-		const data = await fetchWordData(1, $__wordTranslation, $__wordTransliteration);
-		return data;
-	})();
 
 	const tableTitles = {
 		1: { title: 'with the same root' },
@@ -36,54 +30,50 @@
 	}
 </script>
 
-{#await wordData then data}
-	{#if totalAvailableWords > 0}
-		<div class="flex flex-col">
-			<div class="relative space-y-6 sm:rounded-3xl">
-				<h1 class="text-md md:text-2xl text-center">{totalAvailableWords} {totalAvailableWords > 1 ? 'words' : 'word'} {tableTitles[tableType].title}</h1>
-				<div class="max-h-[32em] overflow-auto">
-					<table class="w-full text-sm text-left rtl:text-right rounded-md">
-						<thead class="text-xs uppercase top-0 {window.theme('bgSecondaryLight')}">
-							<tr>
-								<th class="px-6 py-3">#</th>
-								<th class="px-6 py-3">Word</th>
-								<th class="px-6 py-3">Translation</th>
-								<th class="px-6 py-3">Transliteration</th>
-								<th class="px-6 py-3">{term('verse')}</th>
-								<th class="px-6 py-3">Word</th>
+{#if totalAvailableWords > 0}
+	<div class="flex flex-col">
+		<div class="relative space-y-6 sm:rounded-3xl">
+			<h1 class="text-md md:text-2xl text-center">{totalAvailableWords} {totalAvailableWords > 1 ? 'words' : 'word'} {tableTitles[tableType].title}</h1>
+			<div class="max-h-[32em] overflow-auto">
+				<table class="w-full text-sm text-left rtl:text-right rounded-md">
+					<thead class="text-xs uppercase top-0 {window.theme('bgSecondaryLight')}">
+						<tr>
+							<th class="px-6 py-3">#</th>
+							<th class="px-6 py-3">Word</th>
+							<th class="px-6 py-3">Translation</th>
+							<th class="px-6 py-3">Transliteration</th>
+							<th class="px-6 py-3">{term('verse')}</th>
+							<th class="px-6 py-3">Word</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each sanitizedwordKeys.slice(0, lastWordToLoad) as item, i}
+							{@const [chapter, verse, word] = item.split(':')}
+							{@const arabic = wordData.arabicWordData[chapter][verse][0][word - 1]}
+							{@const translation = wordData.translationWordData[chapter][verse][0][word - 1]}
+							{@const transliteration = wordData.transliterationWordData[chapter][verse][0][word - 1]}
+							<tr class="{window.theme('bgMain')} border-b {window.theme('border')} {window.theme('hover')}">
+								<td class="px-6 py-4">{i + 1}</td>
+								<td class="px-6 py-4 text-xl md:text-2xl arabic-font-1">{arabic}</td>
+								<td class="px-6 py-4">{translation}</td>
+								<td class="px-6 py-4">{transliteration}</td>
+								<td class="px-6 py-4">
+									<a class={linkClasses} href="/{chapter}?startVerse={verse}">{chapter}:{verse}</a>
+								</td>
+								<td class="px-6 py-4">
+									<a class={linkClasses} href="/morphology?word={item}">{item}</a>
+								</td>
 							</tr>
-						</thead>
-						<tbody>
-							{#each sanitizedwordKeys.slice(0, lastWordToLoad) as item, i}
-								{@const [chapter, verse, word] = item.split(':')}
-								{@const arabic = data.arabicWordData[chapter][verse][0][word - 1]}
-								{@const translation = data.translationWordData[chapter][verse][0][word - 1]}
-								{@const transliteration = data.transliterationWordData[chapter][verse][0][word - 1]}
-								<tr class="{window.theme('bgMain')} border-b {window.theme('border')} {window.theme('hover')}">
-									<td class="px-6 py-4">{i + 1}</td>
-									<td class="px-6 py-4 text-xl md:text-2xl arabic-font-1">{arabic}</td>
-									<td class="px-6 py-4">{translation}</td>
-									<td class="px-6 py-4">{transliteration}</td>
-									<td class="px-6 py-4">
-										<a class={linkClasses} href="/{chapter}?startVerse={verse}">{chapter}:{verse}</a>
-									</td>
-									<td class="px-6 py-4">
-										<a class={linkClasses} href="/morphology?word={item}">{item}</a>
-									</td>
-								</tr>
-							{/each}
-						</tbody>
-					</table>
-				</div>
-
-				{#if totalAvailableWords > maxResultsToLoad}
-					<div class="text-center text-xs {lastWordToLoad === totalAvailableWords && 'hidden'}">
-						<button on:click={updateLastWordToLoad} class={buttonClasses} data-umami-event="Morphology Load More Button"> Load more </button>
-					</div>
-				{/if}
+						{/each}
+					</tbody>
+				</table>
 			</div>
+
+			{#if totalAvailableWords > maxResultsToLoad}
+				<div class="text-center text-xs {lastWordToLoad === totalAvailableWords && 'hidden'}">
+					<button on:click={updateLastWordToLoad} class={buttonClasses} data-umami-event="Morphology Load More Button"> Load more </button>
+				</div>
+			{/if}
 		</div>
-	{/if}
-{:catch _}
-	<p>error.</p>
-{/await}
+	</div>
+{/if}
