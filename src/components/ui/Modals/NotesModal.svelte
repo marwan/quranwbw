859f35d34540f2020fc64bcb05820a8bfee06c33
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-27 02:51:23 +0530

    delete icon bg change in notes modal

--- src/components/ui/Modals/NotesModal.svelte
+++ src/components/ui/Modals/NotesModal.svelte
@@ -69,7 +69,7 @@
 
 	<div class="flex flex-row">
 		<button on:click={() => updateNote()} class="w-full mr-2 mt-6 {buttonClasses}">Update</button>
-		<button on:click={() => resetNote()} class="w-fit mr-2 mt-6 {buttonOutlineClasses}">
+		<button on:click={() => resetNote()} class="w-fit mt-6 {buttonClasses}">
 			<span><Trash size={5} /></span>
 		</button>
 	</div>
