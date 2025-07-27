9b2b7cd175c5e3abecae3b89b5c351f94c6efcff
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-27 22:32:30 +0530

    another attempt

--- src/components/misc/PageHead.svelte
+++ src/components/misc/PageHead.svelte
@@ -1,28 +1,10 @@
 <script>
 	export let title;
 
-	import { websiteTagline, websiteTitle, websiteURL, wbwLanguages } from '$data/websiteSettings';
-	import { page } from '$app/stores';
+	import { websiteTagline, websiteURL, wbwLanguages } from '$data/websiteSettings';
 </script>
 
 <svelte:head>
-	<!-- HTML Meta Tags -->
 	<title>{title} - {websiteURL}</title>
 	<meta name="description" content="{title} - {websiteTagline} in {wbwLanguages} - {websiteURL}" />
-
-	<!-- Facebook / Open Graph -->
-	<!-- <meta name="og:type" content="website" />
-	<meta name="og:url" content={$page.url.href} />
-	<meta name="og:title" content="{title} - {websiteTagline} in {wbwLanguages}" />
-	<meta name="og:description" content="{websiteTagline} in {wbwLanguages} - {websiteURL}" />
-	<meta property="og:image" content="{$page.url.href}images/icon-512.png" />
-	<meta name="og:site_name" content={websiteTitle} /> -->
-
-	<!-- Twitter Meta Tags -->
-	<!-- <meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:domain" content="quranwbw.com" />
-	<meta name="twitter:url" content={$page.url.href} />
-	<meta name="twitter:title" content="{title} - {websiteTagline}" />
-	<meta name="twitter:description" content="{websiteTagline} in {wbwLanguages} - {websiteURL}" />
-	<meta name="twitter:image" content="{$page.url.href}images/icon-512.png" /> -->
 </svelte:head>
