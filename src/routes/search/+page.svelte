d4a7f8cc29df219ab814e330bebd89b96dc2fb62
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-27 23:40:37 +0530

    revert: moved Kalimat search API call to server route

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -9,6 +9,8 @@
 	import { term } from '$utils/terminologies';
 	import { quranMetaData } from '$data/quranMeta';
 
+	const API_KEY = import.meta.env.VITE_KALIMAT_API_KEY;
+
 	const linkClasses = `w-fit flex flex-row space-x-2 py-4 px-4 rounded-xl items-center cursor-pointer ${window.theme('hoverBorder')} ${window.theme('bgSecondaryLight')}`;
 	const linkTextClasses = 'text-xs md:text-sm text-left w-fit capitalize truncate';
 
@@ -38,7 +40,11 @@
 		}
 
 		try {
-			const response = await fetch(`?query=${encodeURIComponent(searchQuery)}`);
+			const response = await fetch(`https://api.kalimat.dev/search?query=${searchQuery}&numResults=50`, {
+				headers: {
+					'x-api-key': API_KEY
+				}
+			});
 
 			if (response.status !== 200) {
 				badRequest = true;
