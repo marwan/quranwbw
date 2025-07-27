6525ac439ae715083fd9678b7dc1a3c79646741c
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-07-27 22:26:26 +0530

    another attempt

--- src/components/misc/PageHead.svelte
+++ src/components/misc/PageHead.svelte
@@ -11,18 +11,18 @@
 	<meta name="description" content="{title} - {websiteTagline} in {wbwLanguages} - {websiteURL}" />
 
 	<!-- Facebook / Open Graph -->
-	<meta name="og:type" content="website" />
+	<!-- <meta name="og:type" content="website" />
 	<meta name="og:url" content={$page.url.href} />
 	<meta name="og:title" content="{title} - {websiteTagline} in {wbwLanguages}" />
 	<meta name="og:description" content="{websiteTagline} in {wbwLanguages} - {websiteURL}" />
 	<meta property="og:image" content="{$page.url.href}images/icon-512.png" />
-	<meta name="og:site_name" content={websiteTitle} />
+	<meta name="og:site_name" content={websiteTitle} /> -->
 
 	<!-- Twitter Meta Tags -->
-	<meta name="twitter:card" content="summary_large_image" />
+	<!-- <meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:domain" content="quranwbw.com" />
 	<meta name="twitter:url" content={$page.url.href} />
 	<meta name="twitter:title" content="{title} - {websiteTagline}" />
 	<meta name="twitter:description" content="{websiteTagline} in {wbwLanguages} - {websiteURL}" />
-	<meta name="twitter:image" content="{$page.url.href}images/icon-512.png" />
+	<meta name="twitter:image" content="{$page.url.href}images/icon-512.png" /> -->
 </svelte:head>
