f03ca34cfb2adc4264f61afff5b6f25d38188376
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-08-05 23:37:46 +0530

    wip: mushaf book mode

--- src/routes/page/[page]/+page.svelte
+++ src/routes/page/[page]/+page.svelte
@@ -20,18 +20,48 @@
 	import { fetchChapterData, fetchAndCacheJson } from '$utils/fetchData';
 	import '$utils/swiped-events.min.js';
 
+	const pagePairs = {
+		1: [1, 2],
+		2: [1, 2],
+		3: [3, 4],
+		4: [3, 4],
+		5: [5, 6],
+		6: [5, 6],
+		7: [7, 8],
+		8: [7, 8],
+		9: [9, 10],
+		10: [9, 10]
+	};
+
 	// Lines to be centered instead of justified
 	const centeredPageLines = ['528:9', '545:6', '594:5', '602:5', '602:15', '603:10', '603:15', '604:4', '604:9', '604:14', '604:15'];
 
-	let pageData;
-	let startingLine;
-	let endingLine;
-	let chapters = [];
-	let verses = [];
-	let lines = [];
+	let rightPageData;
+
+	let rightPageStartingLine;
+	let rightPageEndingLine;
+
+	let rightPageChapters = [];
+	let rightPageVerses = [];
+	let rightPageLines = [];
+
+	let rightPageNumber;
+	let leftPageNumber;
 
 	// Set the page number
 	$: page = +data.page;
+
+	const bookModeEnabled = false;
+
+	$: if (bookModeEnabled) {
+		rightPageNumber = pagePairs[page][0];
+		leftPageNumber = pagePairs[page][1];
+	} else {
+		rightPageNumber = page;
+		leftPageNumber = page;
+	}
+
+	$: console.log({ page, rightPageNumber, leftPageNumber });
 
 	// Prefetch adjacent pages for better UX
 	$: if ([2, 3].includes($__fontType)) {
@@ -41,63 +71,117 @@
 	}
 
 	$: {
-		chapters = [];
-		verses = [];
-		lines = [];
+		rightPageChapters = [];
+		rightPageVerses = [];
+		rightPageLines = [];
 
-		pageData = (async () => {
-			const data = await fetchVersesByPage(page, selectableFontTypes[$__fontType].id, $__wordTranslation);
+		rightPageData = (async () => {
+			const data = await fetchVersesByPage(rightPageNumber, selectableFontTypes[$__fontType].id, $__wordTranslation);
 			const verseData = data.verses;
-			localStorage.setItem('pageData', JSON.stringify(verseData));
+			localStorage.setItem('rightPageData', JSON.stringify(verseData));
 
 			// Get the first line, of the first word, of the first verse
 			const firstVerse = Object.keys(verseData)[0];
-			startingLine = verseData[firstVerse].words.line[0];
+			rightPageStartingLine = verseData[firstVerse].words.line[0];
 
 			// Get the last line, of the last word, of the last verse
 			const lastVerse = Object.keys(verseData)[Object.keys(verseData).length - 1];
 			const lastWord = verseData[lastVerse].words.line;
-			endingLine = lastWord[lastWord.length - 1];
+			rightPageEndingLine = lastWord[lastWord.length - 1];
 
 			// Get chapter numbers
 			for (const key of Object.keys(verseData)) {
 				const chapter = +key.split(':')[0];
-				if (!chapters.includes(chapter)) {
-					chapters.push(chapter);
+				if (!rightPageChapters.includes(chapter)) {
+					rightPageChapters.push(chapter);
 				}
 			}
 
 			// Get the first verse of each chapter
-			chapters.forEach((chapter) => {
+			rightPageChapters.forEach((chapter) => {
 				for (let verse = 1; verse <= quranMetaData[chapter].verses; verse++) {
 					if (verseData[`${chapter}:${verse}`]) {
-						verses.push(verse);
+						rightPageVerses.push(verse);
 						break;
 					}
 				}
 			});
 
 			// Get line numbers for chapters
-			chapters.forEach((chapter, index) => {
-				lines.push(+verseData[`${chapter}:${verses[index]}`].words.line[0]);
+			rightPageChapters.forEach((chapter, index) => {
+				rightPageLines.push(+verseData[`${chapter}:${rightPageVerses[index]}`].words.line[0]);
 			});
 
-			// Set the mushaf page divisions
-			__mushafPageDivisions.set({
-				chapters: chapters,
-				juz: verseData[Object.keys(verseData)[0]].meta.juz
-			});
+			// // Set the mushaf page divisions
+			// __mushafPageDivisions.set({
+			// 	chapters: chapters,
+			// 	juz: verseData[Object.keys(verseData)[0]].meta.juz
+			// });
 
-			// Update the last read page
-			updateSettings({ type: 'lastRead', value: verseData[Object.keys(verseData)[0]].meta });
+			// // Update the last read page
+			// updateSettings({ type: 'lastRead', value: verseData[Object.keys(verseData)[0]].meta });
 
-			// Event listeners for swipe gestures
-			const pageBlock = document.getElementById('page-block');
-			pageBlock.addEventListener('swiped-left', () => goto(`/page/${page === 1 ? 1 : page - 1}`, { replaceState: false }));
-			pageBlock.addEventListener('swiped-right', () => goto(`/page/${page === 604 ? 604 : page + 1}`, { replaceState: false }));
+			// // Event listeners for swipe gestures
+			// const pageBlock = document.getElementById('page-block');
+			// pageBlock.addEventListener('swiped-left', () => goto(`/page/${page === 1 ? 1 : page - 1}`, { replaceState: false }));
+			// pageBlock.addEventListener('swiped-right', () => goto(`/page/${page === 604 ? 604 : page + 1}`, { replaceState: false }));
 
 			return verseData;
 		})();
+
+		// leftPageData = (async () => {
+		// 	const data = await fetchVersesByPage(leftPageNumber, selectableFontTypes[$__fontType].id, $__wordTranslation);
+		// 	const verseData = data.verses;
+		// 	localStorage.setItem('leftPageData', JSON.stringify(verseData));
+
+		// 	// Get the first line, of the first word, of the first verse
+		// 	const firstVerse = Object.keys(verseData)[0];
+		// 	startingLine = verseData[firstVerse].words.line[0];
+
+		// 	// Get the last line, of the last word, of the last verse
+		// 	const lastVerse = Object.keys(verseData)[Object.keys(verseData).length - 1];
+		// 	const lastWord = verseData[lastVerse].words.line;
+		// 	endingLine = lastWord[lastWord.length - 1];
+
+		// 	// Get chapter numbers
+		// 	for (const key of Object.keys(verseData)) {
+		// 		const chapter = +key.split(':')[0];
+		// 		if (!chapters.includes(chapter)) {
+		// 			chapters.push(chapter);
+		// 		}
+		// 	}
+
+		// 	// Get the first verse of each chapter
+		// 	chapters.forEach((chapter) => {
+		// 		for (let verse = 1; verse <= quranMetaData[chapter].verses; verse++) {
+		// 			if (verseData[`${chapter}:${verse}`]) {
+		// 				verses.push(verse);
+		// 				break;
+		// 			}
+		// 		}
+		// 	});
+
+		// 	// Get line numbers for chapters
+		// 	chapters.forEach((chapter, index) => {
+		// 		lines.push(+verseData[`${chapter}:${verses[index]}`].words.line[0]);
+		// 	});
+
+		// 	// // Set the mushaf page divisions
+		// 	// __mushafPageDivisions.set({
+		// 	// 	chapters: chapters,
+		// 	// 	juz: verseData[Object.keys(verseData)[0]].meta.juz
+		// 	// });
+
+		// 	// // Update the last read page
+		// 	// updateSettings({ type: 'lastRead', value: verseData[Object.keys(verseData)[0]].meta });
+
+		// 	// // Event listeners for swipe gestures
+		// 	// const pageBlock = document.getElementById('page-block');
+		// 	// pageBlock.addEventListener('swiped-left', () => goto(`/page/${page === 1 ? 1 : page - 1}`, { replaceState: false }));
+		// 	// pageBlock.addEventListener('swiped-right', () => goto(`/page/${page === 604 ? 604 : page + 1}`, { replaceState: false }));
+
+		// 	return verseData;
+		// })();
 
 		// Update the page number
 		__pageNumber.set(page);
@@ -180,27 +264,47 @@
 <PageHead title={`Page ${page}`} />
 
 <div id="page-block" class="text-center text-xl mt-6 mb-14 overflow-x-hidden overflow-y-hidden">
-	{#await pageData}
+	{#await rightPageData}
 		<Spinner />
 	{:then}
 		<div class="space-y-2 mt-2.5">
 			<!-- single page -->
-			<div class="max-w-3xl md:max-w-[40rem] pb-2 mx-auto text-[5.4vw] md:text-[36px] lg:text-[36px] {+page === 1 ? 'space-y-1' : 'space-y-2'}">
-				{#each Array.from(Array(endingLine + 1).keys()).slice(startingLine) as line}
-					<!-- show the chapter header if it's the first verse of that chapter -->
-					{#if chapters.length > 0 && lines.includes(line) && verses[lines.indexOf(line)] === 1}
-						<div class="flex flex-col my-2">
-							<ChapterHeader chapter={chapters[lines.indexOf(line)]} />
-							<Bismillah {chapters} {lines} {line} />
-						</div>
-					{/if}
+			<div class="flex flex-row">
+				<!-- right page -->
+				<div class="max-w-3xl md:max-w-[40rem] pb-2 mx-auto text-[5.4vw] md:text-[36px] lg:text-[36px] {+rightPageNumber === 1 ? 'space-y-1' : 'space-y-2'}">
+					{#each Array.from(Array(rightPageEndingLine + 1).keys()).slice(rightPageStartingLine) as line}
+						{#if rightPageChapters.length > 0 && rightPageLines.includes(line) && rightPageVerses[rightPageLines.indexOf(line)] === 1}
+							<div class="flex flex-col my-2">
+								<ChapterHeader chapter={rightPageChapters[rightPageLines.indexOf(line)]} />
+								<Bismillah chapters={rightPageChapters} lines={rightPageLines} {line} />
+							</div>
+						{/if}
 
-					<div class="line {line} flex px-2 arabic-font-{$__fontType} {+page < 3 || centeredPageLines.includes(`${+page}:${line}`) ? 'justify-center' : null} {+page > 2 && !centeredPageLines.includes(`${+page}:${line}`) ? 'justify-between' : null}">
-						{#each Object.entries(JSON.parse(localStorage.getItem('pageData'))) as [key, value]}
-							<WordsBlock {key} {value} {line} />
-						{/each}
-					</div>
-				{/each}
+						<div class="line {line} flex px-2 arabic-font-{$__fontType} {+rightPageNumber < 3 || centeredPageLines.includes(`${+rightPageNumber}:${line}`) ? 'justify-center' : null} {+rightPageNumber > 2 && !centeredPageLines.includes(`${+rightPageNumber}:${line}`) ? 'justify-between' : null}">
+							{#each Object.entries(JSON.parse(localStorage.getItem('rightPageData'))) as [key, value]}
+								<WordsBlock {key} {value} {line} />
+							{/each}
+						</div>
+					{/each}
+				</div>
+
+				<!-- left page -->
+				<!-- <div class="max-w-3xl md:max-w-[40rem] pb-2 mx-auto text-[5.4vw] md:text-[36px] lg:text-[36px] {+page === 1 ? 'space-y-1' : 'space-y-2'}">
+					{#each Array.from(Array(endingLine + 1).keys()).slice(startingLine) as line}
+						{#if chapters.length > 0 && lines.includes(line) && verses[lines.indexOf(line)] === 1}
+							<div class="flex flex-col my-2">
+								<ChapterHeader chapter={chapters[lines.indexOf(line)]} />
+								<Bismillah {chapters} {lines} {line} />
+							</div>
+						{/if}
+
+						<div class="line {line} flex px-2 arabic-font-{$__fontType} {+page < 3 || centeredPageLines.includes(`${+page}:${line}`) ? 'justify-center' : null} {+page > 2 && !centeredPageLines.includes(`${+page}:${line}`) ? 'justify-between' : null}">
+							{#each Object.entries(JSON.parse(localStorage.getItem('pageData'))) as [key, value]}
+								<WordsBlock {key} {value} {line} />
+							{/each}
+						</div>
+					{/each}
+				</div> -->
 			</div>
 
 			<!-- page number -->
