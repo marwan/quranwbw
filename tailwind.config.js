4705e8211173ffb7908cb0f88de991295ba7f8d6
Adham-A <57870101+Adham-A@users.noreply.github.com>
2025-08-31 19:30:21 +0200

    refactor: remove unused generateThemes utility and clean up tailwind colors, remove warning from npm run build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 
+// removing old colors
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'!./src/utils/parseURL.js'
+	],
 	darkMode: 'selector',
 	theme: {
 		extend: {
