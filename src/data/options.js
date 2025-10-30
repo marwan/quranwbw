6354a2aaa53da1d58955364e340c40bc7e0ff853
Adham-A <57870101+Adham-A@users.noreply.github.com>
2025-10-30 22:29:07 +0100

    feat: :sparkles: Add times from 1 to 10 for repeat audio options

--- src/data/options.js
+++ src/data/options.js
@@ -977,4 +977,4 @@ export const selectableAudioDelays = {
 	7: { id: 7, name: 'Audio Length', milliseconds: 999 }
 };
 
-export const selectableRepeatTimes = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+export const selectableRepeatTimes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50];
