454605325f2004242783a045e16ecab5c244cdd2
Adham-A <57870101+Adham-A@users.noreply.github.com>
2025-11-05 18:37:12 +0100

    Phone fix

--- src/components/misc/WordDisplay.svelte
+++ src/components/misc/WordDisplay.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <button
-	class="flex flex-col space-y-1 sm:space-y-2 md:space-y-4 mx-auto items-center mb-3 md:mb-8 pt-1 md:pt-4"
+	class="flex flex-col space-y-6 sm:space-y-8 md:space-y-10 mx-auto items-center mb-3 md:mb-8 pt-1 md:pt-4"
 	on:click={() => playWordAudio({ key: wordKey })}
 >
 	<span class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl arabic-font-1">{word.word_arabic}</span>
