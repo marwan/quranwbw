ecc0164c2ef1f6a58ef58219abd494ac71100a8f
marwan <8071158+marwan@users.noreply.github.com>
2026-02-01 18:15:27 +0530

    Update QuranFontSelector.svelte

--- src/components/ui/SettingsDrawer/QuranFontSelector.svelte
+++ src/components/ui/SettingsDrawer/QuranFontSelector.svelte
@@ -11,12 +11,14 @@
 	// Get downloaded font types from offline settings
 	$: downloadedFontTypes = $__offlineModeSettings?.downloadedDataSettings?.fontTypes ?? [];
 
-	// User online tracker
+	// Network tracker
 	let userOnline = false;
+	let networkCheckPerformed = false;
 
 	// Check online status on component mount
 	onMount(async () => {
 		userOnline = await isUserOnline();
+		networkCheckPerformed = true;
 	});
 
 	// Helper function to check if a font type should be shown
@@ -29,33 +31,44 @@
 			return isAllowedOnPage;
 		}
 
-		console.log(downloadedFontTypes);
-
 		// If user is offline, check both page restrictions AND if font is downloaded
 		const isFontDownloaded = downloadedFontTypes.includes(Number(fontKey));
 		return isAllowedOnPage && isFontDownloaded;
 	}
+
+	// Helper function to check if a type has any visible fonts
+	function hasVisibleFonts(type) {
+		return Object.entries(selectableFontTypes).some(([fontKey, fontType]) => {
+			return fontType.type === type && shouldShowFontType(fontType, fontKey);
+		});
+	}
 </script>
 
-<div class="grid gap-3 w-full">
-	{#each fontTypes as type}
-		<div class="flex flex-col space-y-2 pb-6">
-			<div id="font-type" class="text-md font-medium capitalize">{type}</div>
-			<div id="font-list" class="space-y-3">
-				{#each Object.entries(selectableFontTypes).sort((a, b) => a[1].displayOrder - b[1].displayOrder) as [fontKey, fontType]}
-					{#if fontType.type === type && shouldShowFontType(fontType, fontKey)}
-						<Radio name="fontType" bind:group={$__fontType} value={Number(fontKey)} on:change={(event) => updateSettings({ type: 'fontType', value: +event.target.value })} custom>
-							<div class="{individualRadioClasses} {$__fontType === Number(fontKey) && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">{fontType.font}</div>
+{#if networkCheckPerformed}
+	<div class="grid gap-3 w-full">
+		{#each fontTypes as type}
+			<div class="flex flex-col space-y-2 pb-6">
+				<div id="font-type" class="text-md font-medium capitalize">{type}</div>
+				<div id="font-list" class="space-y-3">
+					{#if hasVisibleFonts(type)}
+						{#each Object.entries(selectableFontTypes).sort((a, b) => a[1].displayOrder - b[1].displayOrder) as [fontKey, fontType]}
+							{#if fontType.type === type && shouldShowFontType(fontType, fontKey)}
+								<Radio name="fontType" bind:group={$__fontType} value={Number(fontKey)} on:change={(event) => updateSettings({ type: 'fontType', value: +event.target.value })} custom>
+									<div class="{individualRadioClasses} {$__fontType === Number(fontKey) && selectedRadioOrCheckboxClasses}">
+										<div class="w-full">{fontType.font}</div>
 
-								{#if $__fontType === Number(fontKey)}
-									<Check size={5} />
-								{/if}
-							</div>
-						</Radio>
+										{#if $__fontType === Number(fontKey)}
+											<Check size={5} />
+										{/if}
+									</div>
+								</Radio>
+							{/if}
+						{/each}
+					{:else}
+						<p class="text-xs opacity-70">No fonts available in this category</p>
 					{/if}
-				{/each}
+				</div>
 			</div>
-		</div>
-	{/each}
-</div>
+		{/each}
+	</div>
+{/if}
