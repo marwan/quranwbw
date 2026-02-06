c69db7ae4b96399189b379828b7583e6ad896423
marwan <8071158+marwan@users.noreply.github.com>
2026-02-06 18:35:23 +0530

    removed unused code

--- src/routes/index/+page.svelte
+++ src/routes/index/+page.svelte
@@ -5,17 +5,12 @@
 	import FullVersesDisplay from '$display/verses/modes/FullVersesDisplay.svelte';
 	import { __currentPage } from '$utils/stores';
 	import { onMount } from 'svelte';
-	import { debounce } from '$utils/debounce';
 	import { staticEndpoint } from '$data/websiteSettings';
 	import { fetchAndCacheJson } from '$utils/fetchData';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 
 	let allIndexes = [];
-	let filteredIndexes = [];
-	let searchQuery = '';
-	let debouncedSearch = '';
-	let isSearching = false;
 	let showScrollTop = false;
 	let selectedTopicId = null;
 	let selectedTopicKeys = '';
@@ -28,7 +23,7 @@
 
 	$: {
 		groupedIndexes = {};
-		for (const item of filteredIndexes) {
+		for (const item of allIndexes) {
 			const index = item.topic;
 			const firstLetter = index[0].toUpperCase();
 			if (!groupedIndexes[firstLetter]) {
@@ -37,26 +32,6 @@
 			groupedIndexes[firstLetter].push(item);
 		}
 	}
-
-	// Debounce handler
-	function updateDebouncedSearch() {
-		isSearching = true;
-		debounce(() => {
-			debouncedSearch = searchQuery.trim().toLowerCase();
-			isSearching = false;
-		}, 500);
-	}
-
-	// Run this whenever searchQuery changes
-	$: searchQuery, updateDebouncedSearch();
-
-	// Filter indexes on debouncedSearch
-	$: filteredIndexes = allIndexes
-		.filter((item) => {
-			const matchesSearch = debouncedSearch ? item.topic.toLowerCase().includes(debouncedSearch) : true;
-			return matchesSearch;
-		})
-		.sort((a, b) => a.topic.localeCompare(b.topic));
 
 	// Watch for URL parameter changes
 	$: if ($page) {
@@ -125,9 +100,8 @@
 			<FullVersesDisplay keys={selectedTopicKeys} />
 		</div>
 	{:else}
-		<!-- Search Input -->
+		<!-- Alphabet Selector -->
 		<div class="my-4">
-			<!-- Alphabet Selector -->
 			<div class="mx-auto flex flex-wrap justify-center px-2">
 				{#each alphabet as letter}
 					<a href="#{letter}" class="ml-1 mt-1 px-2 py-1 rounded-full cursor-pointer no-underline min-w-[2rem] text-center {window.theme('hoverBorder')} {window.theme('bgSecondaryLight')}">
@@ -137,39 +111,25 @@
 			</div>
 		</div>
 
-		<!-- Loading Message -->
-		{#if isSearching}
-			<Spinner />
-		{/if}
-
 		<!-- Indexes Display -->
-		{#if !isSearching}
-			{#if filteredIndexes.length > 0}
-				{#each alphabet as letter}
-					{#if groupedIndexes[letter] && groupedIndexes[letter].length > 0}
-						<div id={letter} class="py-6 border-b {window.theme('border')} scroll-mt-4">
-							<h2 class="text-xl font-bold mb-4 {window.theme('textSecondary')}">
-								{letter}
-							</h2>
-							<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-								{#each groupedIndexes[letter] as item}
-									<button on:click={() => handleTopicClick(item.id)} class="block py-2 rounded-md hover:underline text-left" rel="noopener">
-										{item.topic}
-										<span class={window.theme('textSecondary')}>({item.verses.length})</span>
-									</button>
-								{/each}
-							</div>
+		{#if allIndexes.length > 0}
+			{#each alphabet as letter}
+				{#if groupedIndexes[letter] && groupedIndexes[letter].length > 0}
+					<div id={letter} class="py-6 border-b {window.theme('border')} scroll-mt-4">
+						<h2 class="text-xl font-bold mb-4 {window.theme('textSecondary')}">
+							{letter}
+						</h2>
+						<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+							{#each groupedIndexes[letter] as item}
+								<button on:click={() => handleTopicClick(item.id)} class="block py-2 rounded-md hover:underline text-left" rel="noopener">
+									{item.topic}
+									<span class={window.theme('textSecondary')}>({item.verses.length})</span>
+								</button>
+							{/each}
 						</div>
-					{/if}
-				{/each}
-			{:else}
-				<div class="flex flex-col text-center text-xs space-y-2 max-w-2xl mx-auto mt-4">
-					No results found
-					{#if searchQuery}
-						matching "{searchQuery}"
-					{/if}
-				</div>
-			{/if}
+					</div>
+				{/if}
+			{/each}
 		{/if}
 	{/if}
 </div>
