a28a82839d7f5d5252b07c62fca895978123cff8
marwan <8071158+marwan@users.noreply.github.com>
2026-02-14 23:02:03 +0530

    OT-SVG-LIGHT font test

--- src/utils/getMushafWordFontLink.js
+++ src/utils/getMushafWordFontLink.js
@@ -3,26 +3,27 @@ import { mushafWordFontLink } from '$data/websiteSettings';
 import { selectableThemes } from '$data/options';
 import { __websiteTheme, __fontType } from '$utils/stores';
 
-// Return the Mushaf font URL for a given page
+// Returns the Mushaf word font URL for a given page number
 export function getMushafWordFontLink(page) {
 	const paddedPage = String(page).padStart(3, '0');
+	const os = getOS();
+	const isIOSorMac = os === 'iOS' || os === 'macOS';
+	const isAppleLightThemeWithoutCOLRv1Support = [1, 2, 3].includes(get(__websiteTheme));
 
-	let basePath;
-	let fileName;
-	let fontVersion;
+	// Defaults
+	let basePath = 'COLRv1';
+	let fileName = `QCF4${paddedPage}_COLOR-Regular.woff2`;
+	let fontVersion = 12;
 
-	if (isFirefoxDarkTajweed()) {
+	if (isIOSorMac && isAppleLightThemeWithoutCOLRv1Support) {
+		basePath = 'OT-SVG-LIGHT';
+		fontVersion = 1;
+	} else if (isFirefoxDarkTajweed()) {
 		basePath = 'COLRv1-Dark-FF';
-		fileName = `QCF4${paddedPage}_COLOR-Regular.woff2`;
-		fontVersion = 12;
 	} else if (isFirefoxDarkNonTajweed()) {
 		basePath = 'COLRv1-Dark-FF-Non-Colored';
 		fileName = `QCF4${paddedPage}_X-Regular.woff2`;
 		fontVersion = 10;
-	} else {
-		basePath = 'COLRv1';
-		fileName = `QCF4${paddedPage}_COLOR-Regular.woff2`;
-		fontVersion = 12;
 	}
 
 	return `${mushafWordFontLink}/${basePath}/${fileName}?version=${fontVersion}`;
@@ -47,4 +48,34 @@ export function isFirefoxDarkNonTajweed() {
 
 export function isFirefoxDarkTajweed() {
 	return isMushafFirefoxDark(3);
+}
+
+// Detects the user's operating system using navigator.platform, user-agent client hints (if available), and userAgent fallback.
+// credits: https://dev.to/webs95/detect-macos-ios-windows-android-and-linux-os-with-js-f7n
+function getOS() {
+	const userAgent = navigator.userAgent || '';
+	const platform = navigator?.userAgentData?.platform || navigator.platform || '';
+
+	// iOS detection (some iPadOS versions report as Mac)
+	if (['iPhone', 'iPad', 'iPod'].indexOf(platform) !== -1 || (/Mac/.test(platform) && navigator.maxTouchPoints > 1)) {
+		return 'iOS';
+	}
+
+	if (['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'].indexOf(platform) !== -1) {
+		return 'macOS';
+	}
+
+	if (['Win32', 'Win64', 'Windows', 'WinCE'].indexOf(platform) !== -1) {
+		return 'Windows';
+	}
+
+	if (/Android/i.test(userAgent)) {
+		return 'Android';
+	}
+
+	if (/Linux/i.test(platform)) {
+		return 'Linux';
+	}
+
+	return null;
 }
