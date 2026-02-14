4d42d272eada195dd05500ceabc7629191f704c1
marwan <8071158+marwan@users.noreply.github.com>
2026-02-15 01:42:45 +0530

    better network check in morphology page

--- src/routes/morphology/MorphologyView.svelte
+++ src/routes/morphology/MorphologyView.svelte
@@ -7,15 +7,33 @@
 	import ErrorLoadingData from '$misc/ErrorLoadingData.svelte';
 	import { quranMetaData } from '$data/quranMeta';
 	import { morphologyDataUrls } from '$data/websiteSettings';
-	import { __currentPage, __fontType, __morphologyKey, __wordTranslation, __wordTransliteration } from '$utils/stores';
+	import { __currentPage, __fontType, __morphologyKey, __wordTranslation, __wordTransliteration, __offlineModeSettings } from '$utils/stores';
 	import { buttonClasses, buttonOutlineClasses } from '$data/commonClasses';
 	import { fetchChapterData, fetchAndCacheJson, fetchWordData } from '$utils/fetchData';
 	import { term } from '$utils/terminologies';
 	import { wordAudioController } from '$utils/audioController';
 	import { fade } from 'svelte/transition';
+	import { isUserOnline } from '$utils/serviceWorkerHandler';
+	import { onMount } from 'svelte';
 
 	let chapter, verse, word;
 	let wordRoot = '';
+
+	// Could be either the morphology page or modal
+	$: isMorphologyPage = $__currentPage === 'morphology';
+
+	// Check if the morphology data is downloaded
+	$: isMorphologyDataDownloaded = $__offlineModeSettings?.morphologyData?.downloaded ?? false;
+
+	// Network tracker
+	let userOnline = false;
+	let networkCheckPerformed = false;
+
+	// Check online status on component mount
+	onMount(async () => {
+		userOnline = await isUserOnline();
+		networkCheckPerformed = true;
+	});
 
 	// Extract chapter, verse, and word from the key, defaulting word to 1 if missing or invalid
 	$: {
@@ -99,110 +117,116 @@
 	})();
 </script>
 
-{#await allDataPromise}
-	<Spinner />
-{:then allData}
-	<div class="space-y-6 my-8" in:fade={{ duration: 300 }}>
-		{#if $__currentPage === 'morphology'}
-			<div id="verse-navigator" class="flex flex-row justify-center space-x-8 text-sm">
-				<!-- previous chapter -->
-				{#if verse === 1 && chapter > 1}
-					<a href="/morphology?word={+chapter - 1}:1" class={buttonOutlineClasses}>{@html '&#x2190;'} {term('chapter')} {+chapter - 1}</a>
+{#if networkCheckPerformed}
+	{#if userOnline || isMorphologyDataDownloaded}
+		{#await allDataPromise}
+			<Spinner />
+		{:then allData}
+			<div class="space-y-6 my-8" in:fade={{ duration: 300 }}>
+				{#if isMorphologyPage}
+					<div id="verse-navigator" class="flex flex-row justify-center space-x-8 text-sm">
+						<!-- previous chapter -->
+						{#if verse === 1 && chapter > 1}
+							<a href="/morphology?word={+chapter - 1}:1" class={buttonOutlineClasses}>{@html '&#x2190;'} {term('chapter')} {+chapter - 1}</a>
+						{/if}
+
+						<!-- next verse -->
+						{#if verse > 1}
+							<a href="/morphology?word={chapter}:{+verse - 1}" class={buttonOutlineClasses}>{@html '&#x2190;'} {term('verse')} {chapter}:{+verse - 1}</a>
+						{/if}
+
+						<!-- previous verse -->
+						{#if verse < quranMetaData[chapter].verses}
+							<a href="/morphology?word={chapter}:{+verse + 1}" class={buttonOutlineClasses}>{term('verse')} {chapter}:{+verse + 1} {@html '&#x2192;'}</a>
+						{/if}
+
+						<!-- next chapter -->
+						{#if verse === quranMetaData[chapter].verses && chapter < 114}
+							<a href="/morphology?word={+chapter + 1}:1" class={buttonOutlineClasses}>{term('chapter')} {+chapter + 1} {@html '&#x2192;'}</a>
+						{/if}
+					</div>
 				{/if}
 
-				<!-- next verse -->
-				{#if verse > 1}
-					<a href="/morphology?word={chapter}:{+verse - 1}" class={buttonOutlineClasses}>{@html '&#x2190;'} {term('verse')} {chapter}:{+verse - 1}</a>
-				{/if}
-
-				<!-- previous verse -->
-				{#if verse < quranMetaData[chapter].verses}
-					<a href="/morphology?word={chapter}:{+verse + 1}" class={buttonOutlineClasses}>{term('verse')} {chapter}:{+verse + 1} {@html '&#x2192;'}</a>
-				{/if}
-
-				<!-- next chapter -->
-				{#if verse === quranMetaData[chapter].verses && chapter < 114}
-					<a href="/morphology?word={+chapter + 1}:1" class={buttonOutlineClasses}>{term('chapter')} {+chapter + 1} {@html '&#x2192;'}</a>
-				{/if}
-			</div>
-		{/if}
-
-		<div id="verse">
-			{#if allData.chapterData}
-				<div class="flex flex-wrap justify-center direction-rtl">
-					<WordsBlock key={`${chapter}:${verse}`} value={allData.chapterData} />
-				</div>
-			{:else}
-				<ErrorLoadingData center="false" error="Failed to load verse data" />
-			{/if}
-		</div>
-
-		{#if allData.wordSummaryData && Object.keys(allData.wordSummaryData).length > 0}
-			<div id="word-summary" class="text-center mx-auto md:w-3/4 text-sm md:text-lg pb-6 border-b-2 {window.theme('border')}">
-				<div class="flex flex-col space-y-4">
-					<span>{@html allData.wordSummaryData.data[$__morphologyKey]}</span>
-				</div>
-
-				<!-- Buttons -->
-				<div class="pt-4 flex flex-row justify-center space-x-2 text-xs">
-					<button
-						class={buttonClasses}
-						on:click={() =>
-							wordAudioController({
-								key: allData.wordSummaryData.key,
-								chapter: chapter,
-								verse: verse
-							})}>Play Word</button
-					>
-
-					<!-- Show the "goto verse" button if the user in on morphology page -->
-					{#if $__currentPage === 'morphology'}
-						<a href="/{chapter}/{verse}" class={buttonClasses}>Goto Verse</a>
+				<div id="verse">
+					{#if allData.chapterData}
+						<div class="flex flex-wrap justify-center direction-rtl">
+							<WordsBlock key={`${chapter}:${verse}`} value={allData.chapterData} />
+						</div>
+					{:else}
+						<ErrorLoadingData center="false" error="Failed to load verse data" />
 					{/if}
 				</div>
-			</div>
-		{/if}
 
-		<div id="word-details" class="flex flex-col">
-			<!-- Verbs data -->
-			{#if allData?.wordVerbsData?.data && $__morphologyKey in allData.wordVerbsData.data}
-				<div id="word-forms" class="pb-8 pt-2 border-b-2 {window.theme('border')}">
-					<div class="flex flex-col">
-						<div id="different-verbs">
-							<div class="mx-auto text-center">
-								<div class="relative grid gap-4 grid-cols-2 row-gap-3 md:row-gap-4 md:grid-cols-6">
-									{#each Object.entries(allData.wordVerbsData.data[$__morphologyKey]) as [key, value]}
-										{#if value !== null}
-											<div class="flex flex-col py-5 duration-300 transform {window.theme('bgMain')} border {window.theme('border')} rounded-3xl shadow-sm text-center hover:-translate-y-2">
-												<div class="flex items-center justify-center mb-2">
-													<p id="verb-1" class="text-xl md:text-2xl pb-4 leading-5 arabic-font-1">{value}</p>
-												</div>
-												<p class="text-xs capitalize opacity-70">{key.replace('_', ' ')}</p>
-											</div>
-										{/if}
-									{/each}
+				{#if allData.wordSummaryData && Object.keys(allData.wordSummaryData).length > 0}
+					<div id="word-summary" class="text-center mx-auto md:w-3/4 text-sm md:text-lg pb-6 border-b-2 {window.theme('border')}">
+						<div class="flex flex-col space-y-4">
+							<span>{@html allData.wordSummaryData.data[$__morphologyKey]}</span>
+						</div>
+
+						<!-- Buttons -->
+						<div class="pt-4 flex flex-row justify-center space-x-2 text-xs">
+							<button
+								class={buttonClasses}
+								on:click={() =>
+									wordAudioController({
+										key: allData.wordSummaryData.key,
+										chapter: chapter,
+										verse: verse
+									})}>Play Word</button
+							>
+
+							<!-- Show the "goto verse" button if the user in on morphology page -->
+							{#if isMorphologyPage}
+								<a href="/{chapter}/{verse}" class={buttonClasses}>Goto Verse</a>
+							{/if}
+						</div>
+					</div>
+				{/if}
+
+				<div id="word-details" class="flex flex-col">
+					<!-- Verbs data -->
+					{#if allData?.wordVerbsData?.data && $__morphologyKey in allData.wordVerbsData.data}
+						<div id="word-forms" class="pb-8 pt-2 border-b-2 {window.theme('border')}">
+							<div class="flex flex-col">
+								<div id="different-verbs">
+									<div class="mx-auto text-center">
+										<div class="relative grid gap-4 grid-cols-2 row-gap-3 md:row-gap-4 md:grid-cols-6">
+											{#each Object.entries(allData.wordVerbsData.data[$__morphologyKey]) as [key, value]}
+												{#if value !== null}
+													<div class="flex flex-col py-5 duration-300 transform {window.theme('bgMain')} border {window.theme('border')} rounded-3xl shadow-sm text-center hover:-translate-y-2">
+														<div class="flex items-center justify-center mb-2">
+															<p id="verb-1" class="text-xl md:text-2xl pb-4 leading-5 arabic-font-1">{value}</p>
+														</div>
+														<p class="text-xs capitalize opacity-70">{key.replace('_', ' ')}</p>
+													</div>
+												{/if}
+											{/each}
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-			{/if}
+					{/if}
 
-			<!-- Word with same root -->
-			{#if allData?.wordsWithSameRootData?.data && wordRoot && wordRoot in allData.wordsWithSameRootData.data}
-				<div id="words-with-same-root" class="pb-8 pt-8 border-b-2 {window.theme('border')}">
-					<Table wordKeys={allData.wordsWithSameRootData.data[wordRoot]} tableType={1} wordData={allData.wordData} />
-				</div>
-			{/if}
+					<!-- Word with same root -->
+					{#if allData?.wordsWithSameRootData?.data && wordRoot && wordRoot in allData.wordsWithSameRootData.data}
+						<div id="words-with-same-root" class="pb-8 pt-8 border-b-2 {window.theme('border')}">
+							<Table wordKeys={allData.wordsWithSameRootData.data[wordRoot]} tableType={1} wordData={allData.wordData} />
+						</div>
+					{/if}
 
-			<!-- Exact words in Quran -->
-			{#if Array.isArray(allData?.exactWordsInQuranData) && allData.exactWordsInQuranData.length}
-				<div id="exact-word-data" class="pb-8 pt-8 border-b-2 {window.theme('border')}">
-					<Table wordKeys={allData.exactWordsInQuranData} tableType={2} wordData={allData.wordData} />
+					<!-- Exact words in Quran -->
+					{#if Array.isArray(allData?.exactWordsInQuranData) && allData.exactWordsInQuranData.length}
+						<div id="exact-word-data" class="pb-8 pt-8 border-b-2 {window.theme('border')}">
+							<Table wordKeys={allData.exactWordsInQuranData} tableType={2} wordData={allData.wordData} />
+						</div>
+					{/if}
 				</div>
-			{/if}
-		</div>
-	</div>
-{:catch error}
-	<ErrorLoadingData {error} />
-{/await}
+			</div>
+		{:catch error}
+			<ErrorLoadingData {error} />
+		{/await}
+	{:else}
+		<ErrorLoadingData center={isMorphologyPage} />
+	{/if}
+{/if}
