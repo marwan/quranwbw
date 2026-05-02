24a8297d8c951174fc6b0b66b8baf571d7488abc
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2026-05-02 18:26:20 +0530

    fix: main bottom play button to respect saved audio settings (#436)

--- src/components/ui/BottomToolbar/AudioButton.svelte
+++ src/components/ui/BottomToolbar/AudioButton.svelte
@@ -2,16 +2,13 @@
 	import PlaySolid from '$svgs/PlaySolid.svelte';
 	import PauseSolid from '$svgs/PauseSolid.svelte';
 	import Tooltip from '$ui/FlowbiteSvelte/tooltip/Tooltip.svelte';
+	import { playButtonHandler, setVersesToPlay, resetAudioSettings } from '$utils/audioController';
 	import { __currentPage, __audioSettings, __fullVersesDisplayKeys } from '$utils/stores';
-	import { playVerseAudio, setVersesToPlay, resetAudioSettings } from '$utils/audioController';
 	import { checkOnlineAndAlert } from '$utils/offlineModeHandler';
 
 	// quick play from first verse of page till the max chapter verses
 	async function audioHandler() {
 		if (!(await checkOnlineAndAlert())) return;
-
-		$__audioSettings.language = 'arabic';
-		$__audioSettings.playBoth = false;
 
 		if ($__audioSettings.isPlaying) {
 			resetAudioSettings({ location: 'end' });
@@ -25,11 +22,7 @@
 				setVersesToPlay({ allVersesOnPage: true });
 			}
 
-			playVerseAudio({
-				key: `${window.versesToPlayArray[0]}`,
-				timesToRepeat: 1,
-				language: 'arabic'
-			});
+			playButtonHandler();
 		}
 	}
 </script>
